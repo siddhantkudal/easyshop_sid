22db8dd7870efc19458133da15e92d42e40d10c5
siddhantkudal <kudalsiddhant230@gmail.com>
2025-05-16 01:51:18 +0530

    output file updated these out we reuired in EKS

--- Terraform/ec2/outputs.tf
+++ Terraform/ec2/outputs.tf
@@ -1,3 +1,15 @@
 output "ip_public" {
   value = aws_instance.easyshop_instance[*].public_ip
 }
+
+output "vpcId" {
+    value = aws_vpc.EasyShop.id
+}
+
+output "privatesubnetid" {
+  value = aws_subnet.ES_privatesubnet.id
+}
+
+output "intrasubnetid" {
+    value = aws_subnet.intrasubnet.id
+}
